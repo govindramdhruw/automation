bb9c27a251ed9e07ded8d3c8b336e8328e456036
Govind Ram Dhruw <govindramdhruw2020@gmail.com>
2023-11-22 16:27:43 +0530

    Update main.sh file

--- programs/main.sh
+++ programs/main.sh
@@ -1,1 +1,8 @@
-echo "Hello Shell"
+read -p "Do you want to create new file? y/n" choice
+
+if [ $choice -eq "y" ]; then
+    read -p "Enter your filename? " filename
+    touch $filename
+else
+    echo "Thanks Take care"
+fi
